03233b7f48f46fa031d7231df647ba29901a977c
sska83 <info@a-boz.com>
2016-03-20 22:10:06 +0100

    Initial commit

---
+++ src/com/aboz/trena/ex/Main.java
@@ -0,0 +1,23 @@
+package com.aboz.trena.ex;
+
+public class Main {
+
+    public static void main(String[] args) {
+                // переменные
+                int a = 10; //
+                int b = 1; //
+
+                // обработка исключений
+                try {
+                    int c = a / b; //на 0 не делится
+                }
+                catch (ArithmeticException ex){
+                    System.out.println("не делится на ноль");
+                }
+
+                // за пределами try
+                //int c = a / b;
+                //System.out.println(c);
+
+    }
+}
